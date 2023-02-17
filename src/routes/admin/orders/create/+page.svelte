a528362b1eca3296ffc0131f3a9853d6500e6f0d
Горбулёв Сергей <gorbulevsv@yandex.ru>
2023-02-17 18:23:51 +0300

    7

--- src/routes/admin/orders/create/+page.svelte
+++ src/routes/admin/orders/create/+page.svelte
@@ -4,6 +4,14 @@
 	import { Order } from '$lib/models/Order';
 
 	let order = new Order();
+	let whenTake = {
+		date: new Date().toISOString().slice(0, 10),
+		time: new Date().toTimeString().slice(0, 5)
+	};
+	let whenGive = {
+		date: new Date().toISOString().slice(0, 10),
+		time: new Date().toTimeString().slice(0, 5)
+	};	
 </script>
 
 <PageLayout title="Новый заказ">
@@ -22,14 +30,14 @@
 				<div>откуда:</div>
 				<div class="input-group">
 					<input class="form-control" bind:value={order.from} />
-					<button class="btn btn-dark text-light"><i class="fa-solid fa-earth-americas"></i></button>
+					<button class="btn btn-dark text-light"><i class="fa-solid fa-earth-americas" /></button>
 				</div>
 			</div>
 			<div class="mb-1">
 				<div>куда:</div>
 				<div class="input-group">
 					<input class="form-control" bind:value={order.to} />
-					<button class="btn btn-dark text-light"><i class="fa-solid fa-earth-americas"></i></button>
+					<button class="btn btn-dark text-light"><i class="fa-solid fa-earth-americas" /></button>
 				</div>
 			</div>
 			<div class="d-flex flex-wrap justify-content-between mb-1">
@@ -37,10 +45,10 @@
 					<div class="me-2">когда забирать:</div>
 					<div class="d-flex align-items-end">
 						<div class="flex-grow-1 me-1">
-							<input type="date" class="form-control" bind:value={order.whenTake} />
+							<input type="date" class="form-control" bind:value={whenTake.date} />
 						</div>
 						<div class="flex-grow-1">
-							<input type="time" class="form-control" bind:value={order.whenTake} />
+							<input type="time" class="form-control" bind:value={whenTake.time} />
 						</div>
 					</div>
 				</div>
@@ -49,10 +57,10 @@
 					<div class="me-2">когда доставлять:</div>
 					<div class="d-flex align-items-end">
 						<div class="flex-grow-1 me-1">
-							<input type="date" class="form-control" bind:value={order.whenGive} />
+							<input type="date" class="form-control" bind:value={whenGive.date} />
 						</div>
 						<div class="flex-grow-1">
-							<input type="time" class="form-control" bind:value={order.whenGive} />
+							<input type="time" class="form-control" bind:value={whenGive.time} />
 						</div>
 					</div>
 				</div>
