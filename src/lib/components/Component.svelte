4006820e77f77db2538df29e96ba777fbf9d5fe6
Горбулёв Сергей <gorbulevsv@yandex.ru>
2023-02-17 12:56:13 +0300

    3

---
+++ src/lib/components/Component.svelte
@@ -0,0 +1,8 @@
+<script>
+	export let title = '';
+</script>
+
+<div class="bg-light p-3 rounded mb-3">
+	<h5>{title}</h5>
+	<slot/>
+</div>
--- src/routes/admin/orders/+page.svelte
+++ src/routes/admin/orders/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+	import Component from '$lib/components/Component.svelte';
 	import PageLayout from '$lib/components/PageLayout.svelte';
 </script>
 
@@ -6,4 +7,11 @@
 	<div slot="nav">
 		<button class="btn btn-light text-dark">Создать</button>
 	</div>
+
+	{#each new Array(10) as item}
+		<Component title="Новый заказ">
+			Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam aperiam numquam delectus labore, vero ducimus
+			perferendis deserunt sequi asperiores ipsum vel deleniti pariatur earum? Saepe maiores sequi ut id iste.
+		</Component>
+	{/each}
 </PageLayout>
