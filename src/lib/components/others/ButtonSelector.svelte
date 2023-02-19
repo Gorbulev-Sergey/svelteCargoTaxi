4cffdde6f8362671eec72fb7621e20889804a7dc
Горбулёв Сергей <gorbulevsv@yandex.ru>
2023-02-20 00:36:06 +0300

    1

--- src/lib/components/others/ButtonSelector.svelte
+++ src/lib/components/others/ButtonSelector.svelte
@@ -2,14 +2,20 @@
 	export let placeholder = '';
 	export let titles = new Array();
 	export let selected = 0;
-    export let _class="";
+	export let _class = '';
+	export let onSelected = () => {};
 </script>
 
 <div class="d-inline-flex align-items-center {_class}">
-	<i class="btn btn-sm border-0" style="cursor:default">{placeholder}{placeholder?":":""}</i>
+	<i class="btn btn-sm border-0" style="cursor:default">{placeholder}{placeholder ? ':' : ''}</i>
 	<div class="btn-group btn-group-sm">
 		{#each titles as title, i}
-			<button class="btn btn-{i == selected ? 'dark' : 'light'}" on:click={() => (selected = i)}>{title}</button>
+			<button
+				class="btn btn-{i == selected ? 'dark' : 'light'}"
+				on:click={() => {
+					selected = i;
+					onSelected();
+				}}>{title}</button>
 		{/each}
 	</div>
 </div>
--- src/routes/admin/orders/+page.svelte
+++ src/routes/admin/orders/+page.svelte
@@ -12,11 +12,29 @@
 	import { ordersCount } from '$lib/scripts/storage';
 
 	let orders = new Map();
+	$: ordersFiltered = new Map();
+
+	$: selectedMachDays = 0;
+	$: filterByMachDays = () => {
+		if (selectedMachDays == 0)
+			ordersFiltered = Object.fromEntries(
+				Object.entries(orders).filter(
+					([k, v]) => v.whenTake && v.whenGive && new Date(v.whenTake).toDateString() == new Date(v.whenGive).toDateString()
+				)
+			);
+		else
+			ordersFiltered = Object.fromEntries(
+				Object.entries(orders).filter(
+					([k, v]) => v.whenTake && v.whenGive && new Date(v.whenTake).toDateString() != new Date(v.whenGive).toDateString()
+				)
+			);
+	};
 
 	onMount(async () => {
 		onValue(ref(db, '/orders'), s => {
 			if (s.exists()) {
 				orders = s.val();
+				filterByMachDays();
 				$ordersCount = Object.keys(orders).length.toString();
 			}
 		});
@@ -25,17 +43,24 @@
 
 <PageLayout title="Заказы">
 	<div class=" text-center" slot="center">
-		<ButtonSelector titles={['Однодневные', 'Многодневные']} />
+		<ButtonSelector
+			titles={['Однодневные', 'Многодневные']}
+			bind:selected={selectedMachDays}
+			onSelected={() => {
+				filterByMachDays();
+				console.log(orders);
+				console.log(ordersFiltered);
+			}} />
 		<ButtonSelector titles={['Прошлый месяц', 'Прошлая неделя', 'Вчера', 'Сегодня', 'Завтра', 'Эта неделя', 'Этот месяц']} selected={3} />
 	</div>
 	<div slot="nav">
 		<button class="btn btn-light text-dark" on:click={() => goto('/admin/orders/create')}>Создать</button>
 	</div>
 
-	{#each Object.entries(orders)
+	{#each Object.entries(ordersFiltered)
 		.filter(v => v[1].product)
 		.sort(([k1, v1], [k2, v2]) => new Date(v2.created) - new Date(v1.created)) as [uid, order], i}
-		<Order i={Object.keys(orders).length - i} {uid} {order}>
+		<Order i={Object.keys(ordersFiltered).length - i} {uid} {order}>
 			<div slot="nav" class="d-flex gap-1 flex-column">
 				<button class="btn btn-sm btn-light text-dark" title="редактировать" on:click={() => goto(`/admin/orders/edit/${uid}`)}>
 					<i class="fa-regular fa-pen-to-square" />
