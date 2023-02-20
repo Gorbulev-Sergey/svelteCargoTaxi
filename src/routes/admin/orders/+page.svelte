b2a8e9ba52b2414b1b6eefd7ad41984e82075504
Горбулёв Сергей <gorbulevsv@yandex.ru>
2023-02-20 21:04:51 +0300

    1

--- src/routes/admin/orders/+page.svelte
+++ src/routes/admin/orders/+page.svelte
@@ -11,30 +11,90 @@
 	import { onMount } from 'svelte';
 	import { ordersCount } from '$lib/scripts/storage';
 
-	let orders = new Map();
-	$: ordersFiltered = new Map();
+	$: orders = new Map();
 
-	$: selectedMachDays = 0;
-	$: filterByMachDays = () => {
-		if (selectedMachDays == 0)
-			ordersFiltered = Object.fromEntries(
-				Object.entries(orders).filter(
-					([k, v]) => v.whenTake && v.whenGive && new Date(v.whenTake).toDateString() == new Date(v.whenGive).toDateString()
-				)
-			);
-		else
-			ordersFiltered = Object.fromEntries(
-				Object.entries(orders).filter(
-					([k, v]) => v.whenTake && v.whenGive && new Date(v.whenTake).toDateString() != new Date(v.whenGive).toDateString()
-				)
-			);
+	$: selectedOneManyDays = 0;
+	$: ordersOneManyDays = () => {
+		switch (selectedOneManyDays) {
+			case 0:
+				return Object.fromEntries(
+					Object.entries(orders).filter(
+						([k, v]) => v.whenTake && v.whenGive && new Date(v.whenTake).toDateString() == new Date(v.whenGive).toDateString()
+					)
+				);
+			case 1:
+				return Object.fromEntries(
+					Object.entries(orders).filter(
+						([k, v]) => v.whenTake && v.whenGive && new Date(v.whenTake).toDateString() != new Date(v.whenGive).toDateString()
+					)
+				);
+		}
+	};
+
+	$: selectedTakeGive = 0;
+	$: ordersTakeGive = () => {
+		switch (selectedTakeGive) {
+			case 0:
+				return Object.fromEntries(
+					Object.entries(ordersOneManyDays()).filter(
+						([k, v]) => v.whenTake && new Date(v.whenTake).toDateString() == new Date().toDateString()
+					)
+				);
+			case 1:
+				return Object.fromEntries(
+					Object.entries(ordersOneManyDays()).filter(
+						([k, v]) => v.whenGive && new Date(v.whenGive).toDateString() == new Date().toDateString()
+					)
+				);
+		}
+	};
+
+	$: selectedPrevTodayNext = 3;
+	$: ordersPrevTodayNext = () => {
+		switch (selectedPrevTodayNext) {
+			case 0:
+				// Прошлый месяц
+				return Object.fromEntries(
+					Object.entries(ordersTakeGive()).filter(
+						([k, v]) => new Date(selectedTakeGive == 0 ? v.whenTake : v.whenGive).getMonth() == new Date().getMonth() - 1
+					)
+				);
+			case 2:
+				// Вчера
+				return Object.fromEntries(
+					Object.entries(ordersTakeGive()).filter(
+						([k, v]) => new Date(selectedTakeGive == 0 ? v.whenTake : v.whenGive).getDate() == new Date().getDate() - 1
+					)
+				);
+			case 3:
+				// Сегодня
+				return Object.fromEntries(
+					Object.entries(ordersTakeGive()).filter(
+						([k, v]) => new Date(selectedTakeGive == 0 ? v.whenTake : v.whenGive).getDate() == new Date().getDate()
+					)
+				);
+			case 4:
+				// Завтра
+				return Object.fromEntries(
+					Object.entries(ordersTakeGive()).filter(
+						([k, v]) => new Date(selectedTakeGive == 0 ? v.whenTake : v.whenGive).getDate() == new Date().getDate() + 1
+					)
+				);
+			case 6:
+				// Этот месяц
+				return Object.fromEntries(
+					Object.entries(ordersTakeGive()).filter(
+						([k, v]) => new Date(selectedTakeGive == 0 ? v.whenTake : v.whenGive).getMonth() == new Date().getMonth()
+					)
+				);
+		}
 	};
 
 	onMount(async () => {
 		onValue(ref(db, '/orders'), s => {
 			if (s.exists()) {
 				orders = s.val();
-				filterByMachDays();
+				console.log(new Date().getMonth());
 				$ordersCount = Object.keys(orders).length.toString();
 			}
 		});
@@ -43,24 +103,29 @@
 
 <PageLayout title="Заказы">
 	<div class=" text-center" slot="center">
+		<ButtonSelector titles={['Однодневные', 'Многодневные']} bind:selected={selectedOneManyDays} onSelected={() => {}} />
+		<ButtonSelector titles={['забрать', 'доставить']} bind:selected={selectedTakeGive} />
 		<ButtonSelector
-			titles={['Однодневные', 'Многодневные']}
-			bind:selected={selectedMachDays}
+			titles={['Прошлый месяц', 'Прошлая неделя', 'Вчера', 'Сегодня', 'Завтра', 'Эта неделя', 'Этот месяц']}
+			bind:selected={selectedPrevTodayNext}
 			onSelected={() => {
-				filterByMachDays();
-				console.log(orders);
-				console.log(ordersFiltered);
+				console.log(
+					Object.fromEntries(
+						Object.entries(ordersTakeGive()).filter(
+							([k, v]) => new Date(selectedTakeGive == 0 ? v.whenTake : v.whenGive).getDate() == new Date().getDate()
+						)
+					)
+				);
 			}} />
-		<ButtonSelector titles={['Прошлый месяц', 'Прошлая неделя', 'Вчера', 'Сегодня', 'Завтра', 'Эта неделя', 'Этот месяц']} selected={3} />
 	</div>
 	<div slot="nav">
 		<button class="btn btn-light text-dark" on:click={() => goto('/admin/orders/create')}>Создать</button>
 	</div>
 
-	{#each Object.entries(ordersFiltered)
+	{#each Object.entries(ordersPrevTodayNext())
 		.filter(v => v[1].product)
 		.sort(([k1, v1], [k2, v2]) => new Date(v2.created) - new Date(v1.created)) as [uid, order], i}
-		<Order i={Object.keys(ordersFiltered).length - i} {uid} {order}>
+		<Order i={Object.keys(orders).length - i} {uid} {order}>
 			<div slot="nav" class="d-flex gap-1 flex-column">
 				<button class="btn btn-sm btn-light text-dark" title="редактировать" on:click={() => goto(`/admin/orders/edit/${uid}`)}>
 					<i class="fa-regular fa-pen-to-square" />
