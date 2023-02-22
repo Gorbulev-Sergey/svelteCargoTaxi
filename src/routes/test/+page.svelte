6973f893602bd425ca02d7447251b727b5ba8273
Горбулёв Сергей <gorbulevsv@yandex.ru>
2023-02-22 16:53:40 +0300

    2

---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+	// @ts-nocheck
+
+	class Person {
+		constructor(name = '', lastname = '', age = 0) {
+			this.name = name;
+			this.lastname = lastname;
+			this.age = age;
+		}
+
+        isEquals(pred){
+            if (pred(this) === true) return 'да';
+			else return 'нет';
+        }
+
+		has(pred) {
+            console.log(pred(this));
+			if (pred(this)) return 'содержится';
+			else return 'не содержится';
+		}
+	}
+
+	let person = new Person('Сергей', 'Горбулёв', 20);
+</script>
+
+<div>
+	{person.isEquals(i => i.age == 20)}
+    {person.has(i => i.age)}
+</div>
