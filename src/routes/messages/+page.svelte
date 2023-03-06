a3221623866cf475bc26a3277fbaba8f1716eece
Горбулёв Сергей <gorbulevsv@yandex.ru>
2023-03-06 20:36:11 +0300

    1

--- src/routes/messages/+page.svelte
+++ src/routes/messages/+page.svelte
@@ -3,7 +3,7 @@
 	import PageTitle from '$lib/components/PageTitle.svelte';
 	import { getMessaging, getToken } from 'firebase/messaging';
 
-	getToken(getMessaging(), { vapidKey: 'BOchUxTaicGrSG9sbBqcuk-ALTtc45K1dxzdi0-IdrZoZiTkKlKi_2yqv-MrmgofGa5gV0Relu-6GpCaTaiUJ7Q' }).then(
+	getToken(messaging, { vapidKey: 'BOchUxTaicGrSG9sbBqcuk-ALTtc45K1dxzdi0-IdrZoZiTkKlKi_2yqv-MrmgofGa5gV0Relu-6GpCaTaiUJ7Q' }).then(
 		currentToken => {
 			if (currentToken) console.log('Всё правильно');
 			else console.log('Ошибка');
