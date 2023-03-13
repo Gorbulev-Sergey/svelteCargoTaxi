570f48b1a9c08100dbcf989f04b16a96c7b520fa
Горбулёв Сергей <gorbulevsv@yandex.ru>
2023-03-13 16:02:59 +0300

    2222222222

--- src/routes/messages/+page.svelte
+++ src/routes/messages/+page.svelte
@@ -4,32 +4,62 @@
 	import { getMessaging, getToken } from 'firebase/messaging';
 	import PageLayout from '$lib/components/PageLayout.svelte';
 
-	getToken(messaging, { vapidKey: 'BOchUxTaicGrSG9sbBqcuk-ALTtc45K1dxzdi0-IdrZoZiTkKlKi_2yqv-MrmgofGa5gV0Relu-6GpCaTaiUJ7Q' }).then(
-		currentToken => {
-			if (currentToken) console.log(currentToken);
-		}
-	);
+	// getToken(messaging, { vapidKey: 'BOchUxTaicGrSG9sbBqcuk-ALTtc45K1dxzdi0-IdrZoZiTkKlKi_2yqv-MrmgofGa5gV0Relu-6GpCaTaiUJ7Q' }).then(
+	// 	currentToken => {
+	// 		if (currentToken) console.log(currentToken);
+	// 	}
+	// );
+
+	let message = {
+		theme: 'orders',
+		title: '',
+		text: ''
+	};
 </script>
 
-<PageTitle title="Сообщения" />
-
-<button
-	class="btn btn-light text-dark mt-5"
-	on:click={async () => {
-		// Очень важный код
-		fetch('https://fcm.googleapis.com/fcm/send', {
-			method: 'POST',
-			headers: {
-				'Content-type': 'application/json; charset=UTF-8',
-				Authorization:
-					'key=AAAAwAwwrpU:APA91bFvwPEwjJT9SjMo6u7DPWdB9msy6sbeis6JKvr1V-HBPWMQTv4SfCpLnNmbZSpRUrnFntW0YsRAQGm4t3vlIXKTl5e5tBU_2Fnc5_kQf2afQ9JeTm1WC152sPIDV8u60WGiuANv'
-			},
-			body: JSON.stringify({
-				to: '/topics/orders',
-				notification: {
-					body: 'Появился новый заказ',
-					title: "Подписчикам темы 'orders'"
-				}
-			})
-		});
-	}}>Отправить сообщение</button>
+<div class="container my-3">
+	<div class="bg-light p-3 rounded mt-5">
+		<h2 class="mb-3">Сообщения</h2>
+		<div class="mb-2">
+			<div class="me-2">Тема:</div>
+			<input class="form-control" bind:value={message.theme} />
+		</div>
+		<div class="mb-2">
+			<div class="me-2">Заголовок:</div>
+			<input class="form-control" bind:value={message.title} />
+		</div>
+		<div class="mb-3">
+			<div class="me-2">Текст:</div>
+			<input class="form-control" bind:value={message.text} />
+		</div>
+		<button
+			class="btn btn-dark text-light"
+			on:click={async () => {
+				console.log(message);
+				if (message.theme != '' || message.title != '' || message.text != '')
+					// Очень важный код
+					fetch('https://fcm.googleapis.com/fcm/send', {
+						method: 'POST',
+						headers: {
+							'Content-type': 'application/json; charset=UTF-8',
+							Authorization:
+								'key=AAAAwAwwrpU:APA91bFvwPEwjJT9SjMo6u7DPWdB9msy6sbeis6JKvr1V-HBPWMQTv4SfCpLnNmbZSpRUrnFntW0YsRAQGm4t3vlIXKTl5e5tBU_2Fnc5_kQf2afQ9JeTm1WC152sPIDV8u60WGiuANv'
+						},
+						body: JSON.stringify({
+							to: '/topics/orders',
+							notification: {
+								body: 'Появился новый заказ',
+								title: "Подписчикам темы 'orders'"
+							}
+						})
+					}).then(s => {
+						alert('Сообщение отправлено');
+						message = {
+							theme: 'orders',
+							title: '',
+							text: ''
+						};
+					});
+			}}>Отправить сообщение</button>
+	</div>
+</div>
