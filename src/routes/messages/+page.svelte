09b9ea0a8e4f71a75dc92e59aced1de23995abc7
Горбулёв Сергей <gorbulevsv@yandex.ru>
2023-03-13 15:46:14 +0300

    Работает отправка сообщения

--- src/routes/messages/+page.svelte
+++ src/routes/messages/+page.svelte
@@ -2,6 +2,7 @@
 	import { messaging } from '$lib/scripts/firebase.js';
 	import PageTitle from '$lib/components/PageTitle.svelte';
 	import { getMessaging, getToken } from 'firebase/messaging';
+	import PageLayout from '$lib/components/PageLayout.svelte';
 
 	getToken(messaging, { vapidKey: 'BOchUxTaicGrSG9sbBqcuk-ALTtc45K1dxzdi0-IdrZoZiTkKlKi_2yqv-MrmgofGa5gV0Relu-6GpCaTaiUJ7Q' }).then(
 		currentToken => {
@@ -11,3 +12,23 @@
 </script>
 
 <PageTitle title="Сообщения" />
+
+<button
+	class="btn btn-light text-dark mt-5"
+	on:click={async () => {
+		fetch('https://fcm.googleapis.com/fcm/send', {
+			method: 'POST',
+			headers: {
+				'Content-type': 'application/json; charset=UTF-8',
+				Authorization:
+					'key=AAAAwAwwrpU:APA91bFvwPEwjJT9SjMo6u7DPWdB9msy6sbeis6JKvr1V-HBPWMQTv4SfCpLnNmbZSpRUrnFntW0YsRAQGm4t3vlIXKTl5e5tBU_2Fnc5_kQf2afQ9JeTm1WC152sPIDV8u60WGiuANv'
+			},
+			body: JSON.stringify({
+				to: '/topics/orders',
+				notification: {
+					body: 'Появился новый заказ',
+					title: "Подписчикам темы 'orders'"
+				}
+			})
+		});
+	}}>Отправить сообщение</button>
