6e14b30d36d71bef868f39f71c3097460d91456e
Горбулёв Сергей <gorbulevsv@yandex.ru>
2023-03-17 21:09:55 +0300

    1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,7 @@
 <div class="d-flex flex-column">
 	<div class="sticky-top w-100 bg-light text-dark px-2 py-1">
 		<button class="btn btn-light text-dark me-1" on:click={() => goto('/admin/orders')}>Заказы</button>
-		<button class="btn btn-light text-dark me-1" on:click={() => goto('/messages')}>Сообщения</button>
+		<button class="btn btn-light text-dark me-1" on:click={() => goto('/messages')}>Сообщения сайт</button>
 	</div>
 	<div class="w-100">
 		<div class="container p-3" />
