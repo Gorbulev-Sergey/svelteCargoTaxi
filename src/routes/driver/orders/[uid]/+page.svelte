dce0dece8eb263f13e0b6451b53fd3047dc5a11f
Горбулёв Сергей <gorbulevsv@yandex.ru>
2023-03-23 14:41:32 +0300

    6

--- src/routes/driver/orders/[uid]/+page.svelte
+++ src/routes/driver/orders/[uid]/+page.svelte
@@ -17,47 +17,49 @@
 	});
 </script>
 
-<div id={$page.params.uid} class="d-flex align-items-start bg-light text-dark gap-2 p-2 m-2 rounded">
-	<div class="d-flex flex-column flex-grow-1 me-1">
-		<div class="d-flex justify-content-between w-100">
-			<div>
+{#if order.product}
+	<div id={$page.params.uid} class="d-flex align-items-start bg-light text-dark gap-2 p-2 m-2 rounded">
+		<div class="d-flex flex-column flex-grow-1 me-1">
+			<div class="d-flex justify-content-between w-100">
 				<div>
-					<i>товар:</i> <b>{order.product}</b>
-					{#if order.description}
-						<span class="text-primary">*</span>
+					<div>
+						<i>товар:</i> <b>{order.product}</b>
+						{#if order.description}
+							<span class="text-primary">*</span>
+						{/if}
+					</div>
+					<div><i>откуда:</i> {order.from}</div>
+					<div><i>куда:</i> {order.to}</div>
+					<div><i>забрать:</i> {new Date(order.whenTake).toLocaleString().slice(0, -3)}</div>
+					<div>
+						<i>доставить:</i>
+						{order.hasWhenGiveTime
+							? new Date(order.whenGive).toLocaleString().slice(0, -3)
+							: new Date(order.whenGive).toLocaleString().slice(0, -10)}
+					</div>
+				</div>
+				<div style="min-width: 11em;">
+					{#if order.length}
+						<div><i>длина:</i> {order.length} м</div>
+					{/if}
+					{#if order.width}
+						<div><i>ширина:</i> {order.width} м</div>
+					{/if}
+					{#if order.height}
+						<div><i>высота:</i> {order.height} м</div>
+					{/if}
+					{#if order.count}
+						<div><i>количество:</i> {order.count}</div>
+					{/if}
+					{#if order.totalWeight}
+						<div><i>общая масса:</i> {order.totalWeight} кг</div>
 					{/if}
 				</div>
-				<div><i>откуда:</i> {order.from}</div>
-				<div><i>куда:</i> {order.to}</div>
-				<div><i>забрать:</i> {new Date(order.whenTake).toLocaleString().slice(0, -3)}</div>
-				<div>
-					<i>доставить:</i>
-					{order.hasWhenGiveTime
-						? new Date(order.whenGive).toLocaleString().slice(0, -3)
-						: new Date(order.whenGive).toLocaleString().slice(0, -10)}
-				</div>
 			</div>
-			<div style="min-width: 11em;">
-				{#if order.length}
-					<div><i>длина:</i> {order.length} м</div>
-				{/if}
-				{#if order.width}
-					<div><i>ширина:</i> {order.width} м</div>
-				{/if}
-				{#if order.height}
-					<div><i>высота:</i> {order.height} м</div>
-				{/if}
-				{#if order.count}
-					<div><i>количество:</i> {order.count}</div>
-				{/if}
-				{#if order.totalWeight}
-					<div><i>общая масса:</i> {order.totalWeight} кг</div>
-				{/if}
-			</div>
+			{#if order.description}
+				<div class="text-primary mt-2 border-top"><i>* {order.description}</i></div>
+			{/if}
 		</div>
-		{#if order.description}
-			<div class="text-primary mt-2 border-top"><i>* {order.description}</i></div>
-		{/if}
+		<slot name="nav" />
 	</div>
-	<slot name="nav" />
-</div>
+{/if}
