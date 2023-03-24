3148e21f37e86a30a3f056c511d94715b71bc8e8
Горбулёв Сергей <gorbulevsv@yandex.ru>
2023-03-24 14:40:07 +0300

    1

--- src/routes/driver/+layout.svelte
+++ src/routes/driver/+layout.svelte
@@ -1,16 +1,24 @@
 <script>
 	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
 	import Auth from '$lib/components/Auth.svelte';
+
+	let routes = [
+		{ route: '/driver/orders', title: 'Заказы', icon: 'fa-solid fa-layer-group' },
+		{ route: '/driver/profile', title: 'Профиль', icon: 'fa-regular fa-id-card' },
+	];
 </script>
 
 <Auth signInUrl="/admin/authPhone">
 	<slot />
 	<div class="position-fixed fixed-bottom bottom-0 start-0 w-100">
 		<div class="btn-group w-100 rounded-0">
-			<button class="btn btn-light text-dark w-100 rounded-0 active" on:click={() => goto('/driver/orders')}>
-				<i class="fa-solid fa-layer-group me-1" />Заказы</button>
-			<button class="btn btn-light text-dark w-100 rounded-0" on:click={() => goto('/driver/profile')}>
-				<i class="fa-regular fa-id-card me-1" />Профиль</button>
+			{#each routes as route}
+				<button
+					class="btn w-100 rounded-0 {$page.route.id == route.route ? ' btn-dark text-light' : ' btn-light text-dark'}"
+					on:click={() => goto(route.route)}>
+					<i class="{route.icon} me-1" />{route.title}</button>
+			{/each}
 		</div>
 	</div>
 </Auth>
