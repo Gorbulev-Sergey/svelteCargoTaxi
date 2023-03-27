e4b50ac7f94dd5e4a716c4eb385285198d445f21
Горбулёв Сергей <gorbulevsv@yandex.ru>
2023-03-27 13:33:50 +0300

    new layout

--- src/routes/admin/orders/+layout.svelte
+++ src/routes/admin/orders/+layout.svelte
@@ -1,12 +1,15 @@
 <script>
 	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
 	import Auth from '$lib/components/Auth.svelte';
+	import After from '$lib/components/others/breakepoints/After.svelte';
+	import Before from '$lib/components/others/breakepoints/Before.svelte';
 	import { auth } from '$lib/scripts/firebase';
 	import { driversCount, ordersCount, returnUrl } from '$lib/scripts/storage';
 	import { signOut } from 'firebase/auth';
 
 	let sidbarWidth = '14em';
-	$: menu = [
+	$: routes = [
 		{ title: 'Заказы', url: '/admin/orders', pin: $ordersCount },
 		{ title: 'Водители', url: '', pin: $driversCount },
 		{ title: 'Автомобили', url: '', pin: '12' },
@@ -15,25 +18,51 @@
 </script>
 
 <Auth>
-	<div class="d-flex">
-		<div class="fixed-top bg-light" style="width:{sidbarWidth}; height: 100vh;">
-			<img class="w-100" src="https://sun9-68.userapi.com/JMNL26HgHKZeSqtog_oujmPQwE1glD6Njf02_g/mdS7yp5Eq9o.jpg" alt="" />
-			<div class="mt-4">
-				{#each menu as item}
-					<button class="btn btn-light text-dark rounded-0 mb-1 text-start w-100" on:click={() => goto(item.url)}>
-						<div class="d-flex justify-content-between align-items-end">
-							{item.title}
-							{#if item.pin != ''}
-								<div class="badge bg-dark">{item.pin}</div>
-							{/if}
+	<Before>
+		<div class="sticky-top mb-3 bg-light">
+			<div class="d-flex justify-content-between align-items-center">
+				<button class="btn btn-light bg-light text-dark border-0">Заголовок</button>
+				<div class="dropdown">
+					<button class="btn btn-light m-1" data-bs-toggle="dropdown">Меню</button>
+					<div class="dropdown-menu bg-light shadow-sm border-0">
+						<div>
+							{#each routes as route}
+								<button class="btn btn-light text-dark text-start w-100 rounded-0" on:click={() => goto(route.url)}
+									>{route.title}</button>
+							{/each}
 						</div>
-					</button>
-				{/each}
+					</div>
+				</div>
 			</div>
-			<button class="btn btn-light text-dark rounded-0 mb-1 text-start w-100" on:click={() => signOut(auth)}>Выйти</button>
+			<div class="d-flex justify-content-between align-items-center bg-light p-2">
+				<button class="btn btn-sm btn-dark">1</button>
+				<button class="btn btn-sm btn-dark">2</button>
+			</div>
 		</div>
-		<div class="flex-grow-1" style="margin-left: {sidbarWidth};">
-			<slot />
+		<slot />
+	</Before>
+	<After>
+		<div class="d-flex align-items-start">
+			<div class="sticky-top bg-light" style="width: 15em; min-height: 100vh;">
+				<a href="/admin/orders"
+					><img class="w-100" src="https://sun9-68.userapi.com/JMNL26HgHKZeSqtog_oujmPQwE1glD6Njf02_g/mdS7yp5Eq9o.jpg" alt="" />
+				</a>
+				<div>
+					{#each routes as route}
+						<button class="btn btn-light text-dark text-start w-100 rounded-0" on:click={() => goto(route.url)}
+							>{route.title}</button>
+					{/each}
+				</div>
+			</div>
+			<div class="flex-grow-1 d-flex flex-column gap-3">
+				<div class="sticky-top d-flex justify-content-between align-items-center bg-light p-2 mb3">
+					<button class="btn btn-sm btn-dark">1</button>
+					<button class="btn btn-sm btn-dark">2</button>
+				</div>
+				<div class="px-2">
+					<slot />
+				</div>
+			</div>
 		</div>
-	</div>
+	</After>
 </Auth>
