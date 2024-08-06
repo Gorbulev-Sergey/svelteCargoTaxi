f7ab969538f2141e744038e35a1fc374853c1d3a
Gotbulev-Sergey <gorbulevsv@yandex.ru>
2024-08-06 22:38:54 +0300

    Новая база данных

--- src/lib/scripts/firebase.js
+++ src/lib/scripts/firebase.js
@@ -8,16 +8,14 @@ import { getAuth } from 'firebase/auth';
 //let serviceAccount = require('firebase-adminsdk-4vovk@sveltetaxi.iam.gserviceaccount.com');
 
 const firebaseConfig = {
-	//credential: admin.credential.cert(serviceAccount),
-	apiKey: 'AIzaSyBHEhtO7AtePj-X045LXPL4XS8AC_FDB2Q',
-	authDomain: 'sveltetaxi.firebaseapp.com',
-	databaseURL: 'https://sveltetaxi-default-rtdb.firebaseio.com',
-	projectId: 'sveltetaxi',
-	storageBucket: 'sveltetaxi.appspot.com',
-	messagingSenderId: '824838237845',
-	appId: '1:824838237845:web:3571ca0de5114b0e34f59c',
-	measurementId: 'G-38BQCT4E4Q',
-};
+	apiKey: "AIzaSyD-wJSPHNYP-2673ZjTR_RkdZTT3r0gUus",
+	authDomain: "cargotaxi-b713f.firebaseapp.com",
+	databaseURL: "https://cargotaxi-b713f-default-rtdb.europe-west1.firebasedatabase.app",
+	projectId: "cargotaxi-b713f",
+	storageBucket: "cargotaxi-b713f.appspot.com",
+	messagingSenderId: "257543309523",
+	appId: "1:257543309523:web:ca2dc3d2694c5148b6ec26"
+  };
 
 const app = initializeApp(firebaseConfig);
 export let db = getDatabase(app);
